78e6b12662b658b1e295cf61137d1493487a1d29
anisia1204 <anisia.rosu@student.upt.ro>
2023-11-08 20:26:47 +0200

    backend

---
+++ backend/studbid/src/main/java/com/licenta/studbid/StudbidApplication.java
@@ -0,0 +1,13 @@
+package com.licenta.studbid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudbidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudbidApplication.class, args);
+	}
+
+}
---
+++ backend/studbid/src/test/java/com/licenta/studbid/StudbidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.licenta.studbid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudbidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

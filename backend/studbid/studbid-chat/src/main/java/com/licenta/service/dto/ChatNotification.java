7a906631c604c4948eca1170ed8d479b18d5d644
anisia1204 <anisia.rosu@student.upt.ro>
2024-04-08 02:30:07 +0300

    [backend]: added timestamp field to ChatNotification

--- backend/studbid/studbid-chat/src/main/java/com/licenta/service/dto/ChatNotification.java
+++ backend/studbid/studbid-chat/src/main/java/com/licenta/service/dto/ChatNotification.java
@@ -5,15 +5,17 @@ public class ChatNotification {
     private Long senderId;
     private Long recipientId;
     private String content;
+    private String timestamp;
 
     public ChatNotification() {
     }
 
-    public ChatNotification(String id, Long senderId, Long recipientId, String content) {
+    public ChatNotification(String id, Long senderId, Long recipientId, String content, String timestamp) {
         this.id = id;
         this.senderId = senderId;
         this.recipientId = recipientId;
         this.content = content;
+        this.timestamp = timestamp;
     }
 
     public String getId() {
@@ -46,5 +48,13 @@ public class ChatNotification {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
     }
 }
